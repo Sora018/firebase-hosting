daefaf005449e2caf9052296c21c3254a8f8a0aa
Renan M.C <140671219+Sora018@users.noreply.github.com>
2024-04-29 19:35:41 -0300

    projeto

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGeA6XdR_w8n9ps3RLnq05y8r2Wsq7JIo",
+  authDomain: "bd3-firebase.firebaseapp.com",
+  projectId: "bd3-firebase",
+  storageBucket: "bd3-firebase.appspot.com",
+  messagingSenderId: "558869110898",
+  appId: "1:558869110898:web:910a5b9c54a545334862eb"
+};
+firebase.initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
